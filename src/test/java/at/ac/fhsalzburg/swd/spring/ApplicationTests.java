7b6d0a05b13e4095af83dc136cc3d6c6093574e8
nerovalerius <crypto@mailbox.org>
2020-01-11 14:38:41 +0100

    Added apptests junit

--- src/test/java/at/ac/fhsalzburg/swd/spring/ApplicationTests.java
+++ src/test/java/at/ac/fhsalzburg/swd/spring/ApplicationTests.java
@@ -2,12 +2,15 @@ package at.ac.fhsalzburg.swd.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testAdd() {
+		assertEquals(2, 1 + 1);
 	}
 
 }
+
